5ebc84e99766f096bf6b36049368ed7691ddb57f
Jakub Senko <jsenko@redhat.com>
2020-06-04 02:44:07 +0200

    Implement TLS security support for streams persistence

---
+++ pkg/controller/apicurioregistry/cf_streams_security_tls_ocp.go
@@ -0,0 +1,214 @@
+package apicurioregistry
+
+import (
+	ar "github.com/Apicurio/apicurio-registry-operator/pkg/apis/apicur/v1alpha1"
+	ocp_apps "github.com/openshift/api/apps/v1"
+	core "k8s.io/api/core/v1"
+)
+
+var _ ControlFunction = &StreamsSecurityTLSOcpCF{}
+
+type StreamsSecurityTLSOcpCF struct {
+	ctx                       *Context
+	persistence               string
+	bootstrapServers          string
+	keystoreSecretName        string
+	truststoreSecretName      string
+	valid                     bool
+	foundKeystoreSecretName   string
+	foundTruststoreSecretName string
+	deploymentExists          bool
+	deploymentEntry           ResourceCacheEntry
+}
+
+func NewStreamsSecurityTLSOcpCF(ctx *Context) ControlFunction {
+	return &StreamsSecurityTLSOcpCF{
+		ctx:                       ctx,
+		persistence:               "",
+		bootstrapServers:          "",
+		keystoreSecretName:        "",
+		truststoreSecretName:      "",
+		valid:                     true,
+		foundKeystoreSecretName:   "",
+		foundTruststoreSecretName: "",
+	}
+}
+
+func (this *StreamsSecurityTLSOcpCF) Describe() string {
+	return "StreamsSecurityTLSOcpCF"
+}
+
+func (this *StreamsSecurityTLSOcpCF) Sense() {
+	// Observation #1
+	// Read the config values
+	if specEntry, exists := this.ctx.GetResourceCache().Get(RC_KEY_SPEC); exists {
+		spec := specEntry.GetValue().(*ar.ApicurioRegistry)
+		this.persistence = spec.Spec.Configuration.Persistence
+		this.bootstrapServers = spec.Spec.Configuration.Streams.BootstrapServers
+
+		this.keystoreSecretName = spec.Spec.Configuration.Streams.Security.Tls.KeystoreSecretName
+		this.truststoreSecretName = spec.Spec.Configuration.Streams.Security.Tls.TruststoreSecretName
+	}
+
+	// Observation #2
+	// Deployment exists
+	this.foundKeystoreSecretName = ""
+	this.foundTruststoreSecretName = ""
+
+	deploymentEntry, deploymentExists := this.ctx.GetResourceCache().Get(RC_KEY_DEPLOYMENT_OCP)
+	if deploymentExists {
+		deployment := deploymentEntry.GetValue().(*ocp_apps.DeploymentConfig)
+		for i, v := range deployment.Spec.Template.Spec.Volumes {
+			if v.Name == KEYSTORE_SECRET_VOLUME_NAME {
+				this.foundKeystoreSecretName = deployment.Spec.Template.Spec.Volumes[i].VolumeSource.Secret.SecretName
+			}
+			if v.Name == TRUSTSTORE_SECRET_VOLUME_NAME {
+				this.foundTruststoreSecretName = deployment.Spec.Template.Spec.Volumes[i].VolumeSource.Secret.SecretName
+			}
+		}
+	}
+	this.deploymentExists = deploymentExists
+	this.deploymentEntry = deploymentEntry
+
+	// Observation #3
+	// Validate the config values
+	this.valid = this.persistence == "streams" && this.bootstrapServers != "" &&
+		this.keystoreSecretName != "" && this.truststoreSecretName != ""
+
+	// We won't actively delete old env values if not used
+}
+
+func (this *StreamsSecurityTLSOcpCF) Compare() bool {
+	// Condition #1
+	return this.valid && (
+		this.keystoreSecretName != this.foundKeystoreSecretName ||
+			this.truststoreSecretName != this.foundTruststoreSecretName)
+}
+
+func (this *StreamsSecurityTLSOcpCF) Respond() {
+	this.AddEnv(this.keystoreSecretName, KEYSTORE_SECRET_VOLUME_NAME,
+		this.truststoreSecretName, TRUSTSTORE_SECRET_VOLUME_NAME)
+
+	this.AddSecretVolumePatch(this.deploymentEntry, this.keystoreSecretName, KEYSTORE_SECRET_VOLUME_NAME)
+	this.AddSecretVolumePatch(this.deploymentEntry, this.truststoreSecretName, TRUSTSTORE_SECRET_VOLUME_NAME)
+
+	this.AddSecretMountPatch(this.deploymentEntry, KEYSTORE_SECRET_VOLUME_NAME, "etc/"+KEYSTORE_SECRET_VOLUME_NAME)
+	this.AddSecretMountPatch(this.deploymentEntry, TRUSTSTORE_SECRET_VOLUME_NAME, "etc/"+TRUSTSTORE_SECRET_VOLUME_NAME)
+}
+
+func (this *StreamsSecurityTLSOcpCF) AddEnv(keystoreSecretName string, keystoreSecretVolumeName string,
+	truststoreSecretName string, truststoreSecretVolumeName string) {
+
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_PROPERTIES_PREFIX, "REGISTRY_"))
+
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_TOPOLOGY_SECURITY_PROTOCOL, "SSL"))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_TOPOLOGY_SSL_KEYSTORE_TYPE, "PKCS12"))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_TOPOLOGY_SSL_KEYSTORE_LOCATION,
+		"/etc/"+keystoreSecretVolumeName+"/user.p12"))
+	this.ctx.GetEnvCache().Set(NewEnvCacheEntry(&core.EnvVar{
+		Name: ENV_REGISTRY_STREAMS_TOPOLOGY_SSL_KEYSTORE_PASSWORD,
+		ValueFrom: &core.EnvVarSource{
+			SecretKeyRef: &core.SecretKeySelector{
+				LocalObjectReference: core.LocalObjectReference{
+					Name: keystoreSecretName,
+				},
+				Key: "user.password",
+			},
+		},
+	}))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_TOPOLOGY_SSL_TRUSTSTORE_TYPE, "PKCS12"))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_TOPOLOGY_SSL_TRUSTSTORE_LOCATION,
+		"/etc/"+truststoreSecretVolumeName+"/ca.p12"))
+	this.ctx.GetEnvCache().Set(NewEnvCacheEntry(&core.EnvVar{
+		Name: ENV_REGISTRY_STREAMS_TOPOLOGY_SSL_TRUSTSTORE_PASSWORD,
+		ValueFrom: &core.EnvVarSource{
+			SecretKeyRef: &core.SecretKeySelector{
+				LocalObjectReference: core.LocalObjectReference{
+					Name: truststoreSecretName,
+				},
+				Key: "ca.password",
+			},
+		},
+	}))
+
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SECURITY_PROTOCOL, "SSL"))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SSL_KEYSTORE_TYPE, "PKCS12"))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SSL_KEYSTORE_LOCATION,
+		"/etc/"+keystoreSecretVolumeName+"/user.p12"))
+	this.ctx.GetEnvCache().Set(NewEnvCacheEntry(&core.EnvVar{
+		Name: ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SSL_KEYSTORE_PASSWORD,
+		ValueFrom: &core.EnvVarSource{
+			SecretKeyRef: &core.SecretKeySelector{
+				LocalObjectReference: core.LocalObjectReference{
+					Name: keystoreSecretName,
+				},
+				Key: "user.password",
+			},
+		},
+	}))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SSL_TRUSTSTORE_TYPE, "PKCS12"))
+	this.ctx.GetEnvCache().Set(NewSimpleEnvCacheEntry(ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SSL_TRUSTSTORE_LOCATION,
+		"/etc/"+truststoreSecretVolumeName+"/ca.p12"))
+	this.ctx.GetEnvCache().Set(NewEnvCacheEntry(&core.EnvVar{
+		Name: ENV_REGISTRY_STREAMS_STORAGE_PRODUCER_SSL_TRUSTSTORE_PASSWORD,
+		ValueFrom: &core.EnvVarSource{
+			SecretKeyRef: &core.SecretKeySelector{
+				LocalObjectReference: core.LocalObjectReference{
+					Name: truststoreSecretName,
+				},
+				Key: "ca.password",
+			},
+		},
+	}))
+}
+
+func (this *StreamsSecurityTLSOcpCF) AddSecretVolumePatch(deploymentEntry ResourceCacheEntry, secretName string, volumeName string) {
+	deploymentEntry.ApplyPatch(func(value interface{}) interface{} {
+		deployment := value.(*ocp_apps.DeploymentConfig).DeepCopy()
+		volume := core.Volume{
+			Name: volumeName,
+			VolumeSource: core.VolumeSource{
+				Secret: &core.SecretVolumeSource{
+					SecretName: secretName,
+				},
+			},
+		}
+		j := -1
+		for i, v := range deployment.Spec.Template.Spec.Volumes {
+			if v.Name == volumeName {
+				j = i
+				deployment.Spec.Template.Spec.Volumes[i] = volume
+			}
+		}
+		if j == -1 {
+			deployment.Spec.Template.Spec.Volumes = append(deployment.Spec.Template.Spec.Volumes, volume)
+		}
+		return deployment
+	})
+}
+
+func (this *StreamsSecurityTLSOcpCF) AddSecretMountPatch(deploymentEntry ResourceCacheEntry, volumeName string, mountPath string) {
+	deploymentEntry.ApplyPatch(func(value interface{}) interface{} {
+		deployment := value.(*ocp_apps.DeploymentConfig).DeepCopy()
+		for ci, c := range deployment.Spec.Template.Spec.Containers {
+			if c.Name == this.ctx.GetConfiguration().GetAppName() {
+				mount := core.VolumeMount{
+					Name:      volumeName,
+					ReadOnly:  true,
+					MountPath: mountPath,
+				}
+				j := -1
+				for i, v := range deployment.Spec.Template.Spec.Containers[ci].VolumeMounts {
+					if v.Name == volumeName {
+						j = i
+						deployment.Spec.Template.Spec.Containers[ci].VolumeMounts[i] = mount
+					}
+				}
+				if j == -1 {
+					deployment.Spec.Template.Spec.Containers[ci].VolumeMounts = append(deployment.Spec.Template.Spec.Containers[ci].VolumeMounts, mount)
+				}
+			}
+		}
+		return deployment
+	})
+}
