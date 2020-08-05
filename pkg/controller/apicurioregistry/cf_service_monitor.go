8ca7c1acee988d1d02b52e0038da6c1d2dea32d9
Alexandre Briani Kieling <alex.kieling@gmail.com>
2020-08-05 10:17:35 -0300

    IPT-96 [Monitoring] Create ServiceMonitor for operand metrics (#53)

---
+++ pkg/controller/apicurioregistry/cf_service_monitor.go
@@ -0,0 +1,102 @@
+package apicurioregistry
+
+import (
+	ar "github.com/Apicurio/apicurio-registry-operator/pkg/apis/apicur/v1alpha1"
+	monitoring "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var _ ControlFunction = &ServiceMonitorCF{}
+
+type ServiceMonitorCF struct {
+	ctx                        *Context
+	isServiceMonitorRegistered bool
+	serviceMonitor             *monitoring.ServiceMonitor
+	service                    *core.Service
+}
+
+func NewServiceMonitorCF(ctx *Context) ControlFunction {
+
+	err := ctx.GetController().Watch(&source.Kind{Type: &monitoring.ServiceMonitor{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &ar.ApicurioRegistry{},
+	})
+
+	if err != nil {
+		panic("Error creating ServiceMonitor watch. " + err.Error())
+	}
+
+	return &ServiceMonitorCF{
+		ctx:                        ctx,
+		isServiceMonitorRegistered: false,
+		serviceMonitor:             nil,
+		service:                    nil,
+	}
+}
+
+func (this *ServiceMonitorCF) Describe() string {
+	return "ServiceMonitorCF"
+}
+
+func (this *ServiceMonitorCF) Sense() {
+	monitoringClient := this.ctx.GetClients().Monitoring()
+
+	// Observation #1
+	// Is ServiceMonitor registered?
+	isServiceMonitorRegistered, err := monitoringClient.isServiceMonitorRegistered()
+	if err != nil {
+		log.Error(err, "Could not check ServiceMonitor is registered")
+		return
+	}
+	if !isServiceMonitorRegistered {
+		log.Info("Install prometheus-operator in your cluster to create ServiceMonitor objects")
+	}
+	this.isServiceMonitorRegistered = isServiceMonitorRegistered
+
+	// Observation #2
+	// Get Service
+	serviceEntry, serviceExists := this.ctx.GetResourceCache().Get(RC_KEY_SERVICE)
+	if serviceExists {
+		this.service = serviceEntry.GetValue().(*core.Service)
+	}
+
+	if isServiceMonitorRegistered && serviceExists {
+		// Observation #3
+		// Get ServiceMonitor
+		namespace := this.ctx.configuration.GetAppNamespace()
+		name := this.ctx.configuration.GetAppName()
+		serviceMonitor, err := monitoringClient.GetServiceMonitor(namespace, name)
+		if err != nil {
+			if !errors.IsNotFound(err) {
+				log.Error(err, "Could not get ServiceMonitor")
+			}
+			return
+		}
+		this.serviceMonitor = serviceMonitor
+	}
+}
+
+func (this *ServiceMonitorCF) Compare() bool {
+	// Condition #1
+	// ServiceMonitor is registered
+	// Condition #2
+	// Service has been created
+	// Condition #3
+	// ServiceMonitor has not been created
+	return this.isServiceMonitorRegistered && this.service != nil && this.serviceMonitor == nil
+}
+
+func (this *ServiceMonitorCF) Respond() {
+	monitoringClient := this.ctx.GetClients().Monitoring()
+	monitoringFactory := NewMonitoringFactory(this.ctx)
+	namespace := this.ctx.configuration.GetAppNamespace()
+	serviceMonitor := monitoringFactory.NewServiceMonitor(this.service)
+
+	_, err := monitoringClient.CreateServiceMonitor(namespace, serviceMonitor)
+	if err != nil {
+		log.Error(err, "Could not create ServiceMonitor object")
+	}
+}
