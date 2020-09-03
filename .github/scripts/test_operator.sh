73d0937b448f783d74ac28350c574891256472e7
Fabian Martinez <famartin@redhat.com>
2020-09-03 13:37:13 +0200

    run tests on PR

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -e -a
 
 git clone git@github.com:Apicurio/apicurio-registry-k8s-tests-e2e.git
 
@@ -7,6 +7,10 @@ pushd apicurio-registry-k8s-tests-e2e
 
 ./scripts/install_kind.sh
 
+OPERATOR_METADATA_IMAGE=docker.io/apicurio/apicurio-registry-operator-metadata:latest-dev
+
 make run-operator-ci
 
 popd
+
+set +e +a
