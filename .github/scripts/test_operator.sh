69c25c90a7f0c1459ed7a476e490e6346d715f2d
Fabian Martinez <famartin@redhat.com>
2020-09-04 10:21:16 +0200

    parametrize bundle url

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e -a
 
+OPERATOR_METADATA_IMAGE=docker.io/apicurio/apicurio-registry-operator-metadata:latest-dev
+BUNDLE_URL=${PWD}/docs/resources/install.yaml
+
 git clone https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git
 
 pushd apicurio-registry-k8s-tests-e2e
 
 ./scripts/install_kind.sh
-
-OPERATOR_METADATA_IMAGE=docker.io/apicurio/apicurio-registry-operator-metadata:latest-dev
 
 make run-operator-ci
 
