5c40c6f29f0f8465bb2a4c60e77745b028c488a9
Jakub Senko <jsenko@redhat.com>
2020-11-19 00:04:49 +0100

    Revert "Temporary testing setup"
    This reverts commit 843d3031a2e3f04e0e8ca6a1334c4886a8a26c34.

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -a
 
-OPERATOR_METADATA_IMAGE=docker.io/apicurio/apicurio-registry-operator-metadata:latest
+OPERATOR_METADATA_IMAGE=docker.io/apicurio/apicurio-registry-operator-metadata:latest-dev
 BUNDLE_URL=${PWD}/docs/resources/install-dev.yaml
 
 git clone https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git
