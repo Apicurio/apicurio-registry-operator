c745b8377f29f7c10cb11ace21d78af7220c96d3
Jakub Senko <jsenko@redhat.com>
2020-11-18 15:33:30 +0100

    Create install-dev.yaml - install.yaml is used for stable releases

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -2,7 +2,7 @@
 set -e -a
 
 OPERATOR_METADATA_IMAGE=docker.io/apicurio/apicurio-registry-operator-metadata:latest-dev
-BUNDLE_URL=${PWD}/docs/resources/install.yaml
+BUNDLE_URL=${PWD}/docs/resources/install-dev.yaml
 
 git clone https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git
 
