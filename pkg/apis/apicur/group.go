49ab0ae84231ebb4fdfc7032310204330785abe0
Jakub Senko <jsenko@redhat.com>
2021-03-24 14:01:28 +0100

    (Base) Delete old content

--- pkg/apis/apicur/group.go
+++
@@ -1,6 +0,0 @@
-// Package registry contains registry API versions.
-//
-// This file ensures Go source parsers acknowledge the registry package
-// and any child packages. It can be removed if any other Go source files are
-// added to this package.
-package apicur
--- pkg/apis/apicur/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// Package v1alpha1 contains API Schema definitions for the registry v1alpha1 API group
-// +k8s:deepcopy-gen=package,register
-// +groupName=apicur.io
-package v1alpha1
