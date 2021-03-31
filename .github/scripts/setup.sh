5fbb006f90eed2aa9ba001238f2842d0148e68b6
Fabian Martinez <famartin@redhat.com>
2021-03-31 11:50:49 +0200

    fix setup script

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -6,7 +6,7 @@ export GO111MODULE="on"
 
 echo "Install Operator SDK"
 export OPERATOR_SDK_VERSION="1.4.2"
-curl -fsSL "https://github.com/operator-framework/operator-sdk/releases/download/v$OPERATOR_SDK_VERSION/operator-sdk-v$OPERATOR_SDK_VERSION-x86_64-linux-gnu" > operator-sdk
+curl -fsSL "https://github.com/operator-framework/operator-sdk/releases/download/v$OPERATOR_SDK_VERSION/operator-sdk_linux_amd64" > operator-sdk
 chmod +x operator-sdk
 sudo mv operator-sdk /usr/local/bin/operator-sdk
 
