2e4c96bde2ad57fefa1b225fd70df51f7d26eef5
Fabian Martinez <famartin@redhat.com>
2021-03-31 11:45:16 +0200

    updates in scripts and workflows

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -5,12 +5,14 @@ VERSION=$(sed -n 's/^.*Version.*=.*"\(.*\)".*$/\1/p' ./version/version.go)
 
 echo $VERSION
 
-OPERATOR_IMAGE="${IMAGE_REGISTRY}/${IMAGE_REGISTRY_ORG}/apicurio-registry-operator:$VERSION"
-OPERATOR_METADATA_IMAGE="${IMAGE_REGISTRY}/${IMAGE_REGISTRY_ORG}/apicurio-registry-operator-metadata:$VERSION"
+OPERATOR_IMAGE="quay.io/apicurio/apicurio-registry-operator:$VERSION"
+OPERATOR_METADATA_IMAGE="quay.io/apicurio/apicurio-registry-operator-bundle:$VERSION"
 
-BUNDLE_URL=${PWD}/docs/resources/install-dev.yaml
+BUNDLE_URL=${PWD}/dist/default-install.yaml
 
-git clone https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git
+make dist
+
+git clone -b update-operator-v2 https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git
 
 pushd apicurio-registry-k8s-tests-e2e
 
