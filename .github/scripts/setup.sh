7b646cdbaa77493f844973e1e489942c81986f84
Jakub Senko <jsenko@redhat.com>
2022-09-14 02:02:33 +0200

    refactor: update Makefile, make sure catalog image is built properly

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -4,21 +4,5 @@ set -e
 echo "Set up golang"
 export GO111MODULE="on"
 
-echo "Install Operator SDK"
-export OPERATOR_SDK_VERSION="1.17.0"
-curl -fsSL "https://github.com/operator-framework/operator-sdk/releases/download/v$OPERATOR_SDK_VERSION/operator-sdk_linux_amd64" >operator-sdk
-chmod +x operator-sdk
-sudo mv operator-sdk /usr/local/bin/operator-sdk
-
 echo "Clean cache"
 sudo apt-get clean
-
-echo "Install Antora"
-sudo npm i -g @antora/cli @antora/site-generator
-antora -v
-
-echo "Install 'yq' v4.x"
-export YQ_VERSION="v4.9.2"
-curl -fsSL "https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/yq_linux_amd64" >yq
-chmod +x yq
-sudo mv yq /usr/local/bin/yq
