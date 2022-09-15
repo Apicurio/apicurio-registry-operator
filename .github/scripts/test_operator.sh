4ada45096542acd8c821883e0e8048a12536e8d8
Jakub Senko <jsenko@redhat.com>
2022-09-16 00:24:23 +0200

    chore: simplify PR workflow, fix config property

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -8,7 +8,7 @@ echo $VERSION
 echo $DASH_VERSION
 
 OPERATOR_IMAGE="quay.io/apicurio/apicurio-registry-operator:$VERSION"
-CSV_VERSION=v1.1.0-dev-v2.x
+CSV_VERSION=1.1.0-dev-v2.x
 OPERATOR_METADATA_IMAGE="quay.io/apicurio/apicurio-registry-operator-bundle:$CSV_VERSION"
 OLM_CSV="apicurio-registry-operator.v$CSV_VERSION"
 CATALOG_SOURCE_IMAGE="quay.io/apicurio/apicurio-registry-operator-catalog:latest$DASH_VERSION"
