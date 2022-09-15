c1b91c4441439d2bb85804e3fcba2af2a5bed20d
Jakub Senko <jsenko@redhat.com>
2022-09-15 23:07:16 +0200

    chore: adjust test config

--- .github/scripts/test_operator.sh
+++ .github/scripts/test_operator.sh
@@ -2,11 +2,15 @@
 set -e -a
 
 VERSION=$(sed -n 's/^.*Version.*=.*"\(.*\)".*$/\1/p' ./version/version.go)
+DASH_VERSION = $(shell echo "$(VERSION)" | sed -n 's/^[0-9\.]*-\([^-+]*\).*$$/-\1/p')
 
 echo $VERSION
+echo $DASH_VERSION
 
 OPERATOR_IMAGE="quay.io/apicurio/apicurio-registry-operator:$VERSION"
 OPERATOR_METADATA_IMAGE="quay.io/apicurio/apicurio-registry-operator-bundle:$VERSION"
+OLM_CSV=apicurio-registry-operator.v1.1.0-dev-v2.x
+CATALOG_SOURCE_IMAGE="quay.io/apicurio/apicurio-registry-operator-catalog:latest$DASH_VERSION"
 
 BUNDLE_URL=${PWD}/dist/install.yaml
 OPERATOR_PROJECT_DIR=${PWD}
